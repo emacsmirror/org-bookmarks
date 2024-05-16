cdd57bf2aee9a26883312a2c5e48701e4b60802f
stardiviner <numbchild@gmail.com>
2024-05-16 13:39:31 +0800

    Bump version to v1.1

--- org-bookmarks.el
+++ org-bookmarks.el
@@ -5,7 +5,7 @@
 
 ;; Authors: stardiviner <numbchild@gmail.com>
 ;; Package-Requires: ((emacs "26.1"))
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: outline matching hypermedia org
 ;; URL: https://repo.or.cz/org-bookmarks.git
 
