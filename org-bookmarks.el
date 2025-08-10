a2506a677c2f66d6ba7f72857131a5fe25742607
stardiviner <numbchild@gmail.com>
2025-08-10 22:44:35 +0800

    reset the whole database cache variable when none file select

--- org-bookmarks.el
+++ org-bookmarks.el
@@ -262,14 +262,17 @@ The candidates-data is from function `org-bookmarks--return-candidates'.")
   (message "[org-bookmarks] database cache updated!"))
 
 (defun org-bookmarks-db-reset (&optional file)
-  "Reset the `org-bookmarks' database cache for FILE."
+  "Reset the `org-bookmarks' database cache for selected FILE.
+Reset the whole database cache variable when none file select."
   (interactive (list (expand-file-name
                       (completing-read "[org-bookmarks] update db cache from file: "
                                        (list org-bookmarks-file
                                              (buffer-file-name (current-buffer)))))))
-  (let* ((file (expand-file-name (or file org-bookmarks-file))))
-    (setf (alist-get file org-bookmarks--candidates-cache-alist nil nil 'string-equal) nil)
-    (message "[org-bookmarks] database reset for file <%s> records" file)))
+  (if-let* ((file (expand-file-name (or file org-bookmarks-file))))
+      (progn
+        (setf (alist-get file org-bookmarks--candidates-cache-alist nil nil 'string-equal) nil)
+        (message "[org-bookmarks] database reset for file <%s> records" file))
+    (setf org-bookmarks--candidates-cache-alist nil)))
 
 ;;; Auto update org-bookmarks database cache in Emacs idle timer.
 (defcustom org-bookmarks-db-update-idle-interval (* 60 10)
